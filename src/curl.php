b9badc13ac41189072c057c9ba2435412ee1948c
xiaoqiuqiu <735554270@qq.com>
2022-01-11 17:55:47 +0800

    add

---
+++ src/curl.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace kiwi\curl;
+
+class curl{
+
+    public function curl_get($url){
+
+        $header = array(
+            'Accept: application/json',
+        );
+        $curl = curl_init();
+        //设置抓取的url
+        curl_setopt($curl, CURLOPT_URL, $url);
+        //设置头文件的信息作为数据流输出
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        // 超时设置,以秒为单位
+        curl_setopt($curl, CURLOPT_TIMEOUT, 1);
+
+        // 超时设置，以毫秒为单位
+        curl_setopt($curl, CURLOPT_TIMEOUT_MS, 1000 * 60);
+        // 301 跟随
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        // 设置请求头
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+        //设置获取的信息以文件流的形式返回，而不是直接输出。
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        //执行命令
+        $data = curl_exec($curl);
+
+        // 显示错误信息
+        if (curl_error($curl)) {
+            curl_close($curl);
+            return "Error: " . curl_error($curl);
+        } else {
+            // 打印返回的内容
+            curl_close($curl);
+        }
+        return $data;
+    }
+
+}
